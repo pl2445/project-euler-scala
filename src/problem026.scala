17887e51738e1f5769ae77a2202af8c3422ce1c6
lp <pl2445@gmail.com>
2021-06-20 22:03:20 -0700

    adding wiki

--- src/problem026.scala
+++ src/problem026.scala
@@ -1,7 +1,8 @@
 object problem026 {
+  //https://en.wikipedia.org/wiki/Repeating_decimal
   def main(args: Array[String]): Unit = {
     val ps =
-      (2 until 1000).map(i => (1 to 2000).find(BigInt(10).modPow(_, i) == 1))
+      (2 until 1000).map(i => (1 to 1000).find(BigInt(10).modPow(_, i) == 1))
 
     val r = 2 + ps.indexOf(Some(ps.flatten.max))
     println(r)
