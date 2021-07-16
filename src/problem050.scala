ba4d4cfae8299a47dceae4bb28c698217833d760
Peng <pl2445@gmail.com>
2021-07-16 07:10:49 -0700

    first 50! 20210716

--- src/problem050.scala
+++ src/problem050.scala
@@ -3,6 +3,18 @@ object problem050 {
     .from(3)
     .filter(i => primes.takeWhile(j => j * j <= i).forall(i % _ > 0))
   def main(args: Array[String]): Unit = {
-    println(primes.takeWhile(_ < 1000000).toList.size)
+    val p = primes.takeWhile(_ < 1000000).toList
+    val upperLimit =
+      (1 to 10000).find(n => p.take(n).sum >= 1000000).get
+    val r = (1 to upperLimit)
+      .map(n => {
+        val s = p
+          .sliding(n)
+          .takeWhile(_.sum < 1000000)
+          .find(lst => p.contains(lst.sum))
+        if (s.isDefined) (n, s.get.sum) else (-1, -1)
+      })
+      .filter(t => t._1 > 0)
+    println(r)
   }
 }
